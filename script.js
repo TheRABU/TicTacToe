6736a0ca91d6c6dbde50adf5b772248333f04baf
Md Sifatul Islam Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-08-19 11:42:13 +0600

    rebuild

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
 const WINNING_COMBINATIONS = [
     [0, 1, 2],
     [3, 4, 5],
@@ -9,99 +7,139 @@ const WINNING_COMBINATIONS = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-]
-const cellElements = document.querySelectorAll('[data-cell]')
-const board = document.getElementById('board')
-const winningMessageElement = document.getElementById('winningMessage')
-const restartButton = document.getElementById('restartButton')
-const winningMessageTextElement = document.querySelector('[data-winnig-message-text]')
+];
+const SYMBOLS = {
+    x: "x",
+    o: "o",
+};
 
-let circleTurn
+const startBtn = document.getElementById("startBtn");
+const gameBoard = document.getElementById("board");
 
+class Game {
+    /**
+     * @type {NodeListOf<HTMLDivElement>}
+     */
+    cellElements;
+    /**
+     * @type {HTMLButtonElement}
+     */
+    msgEl;
+    /**
+     * @type {"x" | "o" | null}
+     */
+    winningSymbol = null;
+    /**
+     * @type {[key: number, value: string][]}
+     */
+    board = [];
+    circleTurn = false;
+    gameOver = false;
 
-startGame()
-
-restartButton.addEventListener('click', startGame)
-
-function startGame() {
-    circleTurn = false
-    cellElements.forEach(cell => {
-        cell.classList.remove(X_CLASS)
-        cell.classList.remove(CIRCLE_CLASS)
-        cell.removeEventListener('click', handleClick)
-        cell.addEventListener('click', handleClick, { once: true })
-    })
-    setBoardHoverClass()
-    winningMessageElement.classList.remove('show')
-}
-
-
-
-function handleClick(e) {
-    const cell = e.target
-    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-    placeMark(cell, currentClass)
-    if (checkWin(currentClass)) {
-        endGame(false)
-    } else if (isDraw()) {
-        endGame(true)
-    } else {
-        swapTurns()
-        setBoardHoverClass()
-        console.log('Clicked')
-    }
-    // placemarks
-    //check for wins
-    // check for draw
-    // swtich turns
-
-}
-
-function endGame(draw) {
-    if (draw) {
-        winningMessageElement.innerText = "Draw!"
-    } else {
-        winningMessageTextElement.innerText = `${circleTurn ? " O's : X's" : Wins!}
-    }
-    winningMessagElement.classList.add('show')
-}
-
-function isDraw() {
-    return [...cellElements].every(cell => {
-        return cell.classList.contains(X_CLASS) ||
-            cell.classList.contains(CIRCLE_CLASS)
-    })
-}
-
-
-
-function placeMark(cell, currentClass) {
-    cell.classList.add(currentClass)
-}
-
-function swapTurns() {
-    circleTurn != circleTurn
-}
-
-function setBoardHoverClass() {
-    board.classList.remove(X_CLASS)
-    board.classList.remove(CIRCLE_CLASS)
-    if (circleTurn) {
-        board.classList.add(CIRCLE_CLASS)
-    } else {
-        board.classList.add(X_CLASS)
+    /**
+     *
+     * @param {HTMLButtonElement} startBtn
+     */
+    constructor() {
+        this.cellElements = document.querySelectorAll("[data-cell]");
+        this.msgEl = document.getElementById("msg");
+        this.setBoard();
     }
 
-}
+    start() {
+        gameBoard.classList.remove("hidden");
+        gameBoard.classList.add("board");
+        this.msgEl.innerText = "";
+        this.scaffoldSymbols();
+        this.setBoard();
+        this.gameOver = false;
+        this.circleTurn = false;
+        this.winningSymbol = null;
+        this.cellElements.forEach((el, i) => {
+            el.classList.remove("set");
+            el.addEventListener("click", (e) => this.handleSelectCell(e, i));
+        });
+    }
 
-function checkWin(currentClass) 
-{
-    return WINNING_COMBINATIONS.some(combination => 
-        {
-            return combination.every(index =>) 
-            {
-                return cellElements[index].classList.contains(currentClass)
+    /**
+     * @private
+     */
+    setBoard() {
+        this.board = [];
+        for (let i = 0; i < 9; i++) {
+            this.board.push([i, ""]);
+        }
+    }
+
+    /**
+     * @private
+     * @type {(e: MouseEvent) => void}
+     */
+    handleSelectCell(e, i) {
+        if (!this.board[i][1]) {
+            const symbol = this.circleTurn ? SYMBOLS.o : SYMBOLS.x;
+            this.board[i][1] = symbol;
+            e.currentTarget.dataset.cell = symbol;
+            e.currentTarget.classList.add("set");
+            this.circleTurn = !this.circleTurn;
+            this.scaffoldSymbols();
+            this.matchWinningCombinations();
+        }
+
+        if (this.gameOver) {
+            if (this.winningSymbol) {
+                this.msgEl.innerText = this.winningSymbol.toUpperCase() + " wins!";
+            } else {
+                this.msgEl.innerText = "No one wins!";
             }
-    
-        })
+        }
+    }
+
+    /**
+     * @private
+     */
+    scaffoldSymbols() {
+        this.cellElements.forEach((el) => {
+            el.dataset.cellHover = this.circleTurn ? SYMBOLS.o : SYMBOLS.x;
+        });
+    }
+
+    /**
+     * @private
+     */
+    matchWinningCombinations() {
+        WINNING_COMBINATIONS.forEach((cmb) => {
+            const matches = [];
+            cmb.forEach((i) => {
+                matches.push(this.board[i][1]);
+            });
+            if (matches[0] === SYMBOLS.x && matches[1] === SYMBOLS.x && matches[2] === SYMBOLS.x) {
+                console.log("X Wins");
+                this.winningSymbol = SYMBOLS.x;
+                this.gameOver = true;
+                return;
+            } else if (matches[0] === SYMBOLS.o && matches[1] === SYMBOLS.o && matches[2] === SYMBOLS.o) {
+                console.log("O Wins");
+                this.winningSymbol = SYMBOLS.o;
+                this.gameOver = true;
+                return;
+            }
+        });
+
+        let filledCells = 0;
+        this.board.forEach((cel) => {
+            if (cel[1]) {
+                filledCells++;
+            }
+        });
+        if (filledCells === 9) {
+            this.gameOver = true;
+        }
+    }
 }
+
+const game = new Game();
+startBtn.addEventListener("click", (e) => {
+    e.currentTarget.innerText = "Restart";
+    game.start();
+});
